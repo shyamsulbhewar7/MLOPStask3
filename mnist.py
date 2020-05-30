190dd1245a82698d7b8a4ee7623d3c6703776c31
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-05-31 01:20:36 +0530

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -28,15 +28,15 @@ from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling
 from keras.layers.normalization import BatchNormalization
 import numpy as np
 model = Sequential()
-model.add(Conv2D(filters= 64, kernel_size=(7,7), strides=(1,1), padding='valid'))
+model.add(Conv2D(filters= 64, kernel_size=(7,7), strides=(), padding='valid'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-model.add(Conv2D(filters= 64, kernel_size=(3,3), strides=(1,1), padding='valid'))
-model.add(Conv2D(filters= 128, kernel_size=(2,2), strides=(1,1), padding='valid'))
+model.add(Conv2D(filters= 64, kernel_size=(3,3), strides=(3,3), padding='valid'))
+model.add(Conv2D(filters= 128, kernel_size=(2,2), strides=(2,2), padding='valid'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-model.add(Conv2D(filters= 256, kernel_size=(2,2), strides=(1,1), padding='valid'))
-model.add(Conv2D(filters= 512, kernel_size=(2,2), strides=(1,1), padding='valid'))
+model.add(Conv2D(filters= 256, kernel_size=(2,2), strides=(2,2), padding='valid'))
+model.add(Conv2D(filters= 512, kernel_size=(2,2), strides=(2,2), padding='valid'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-model.add(Conv2D(filters= 1024, kernel_size=(2,2), strides=(1,1), padding='valid'))
+model.add(Conv2D(filters= 1024, kernel_size=(2,2), strides=(2,2), padding='valid'))
 model.add(Dense(4096, input_shape=(224*224*3,)))
 model.add(Activation('relu'))
 # Add Dropout

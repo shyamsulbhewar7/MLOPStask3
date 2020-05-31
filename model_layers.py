a9250634389c83bc3ea7d12f844dfe64d1e37f70
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-05-31 20:02:53 +0530

    Update model_layers.py

--- model_layers.py
+++ model_layers.py
@@ -15,6 +15,4 @@ model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform',
 model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
 model.add(MaxPooling2D((2, 2)))
-	#model.add(Flatten()) #removing this
-	
-  #model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+

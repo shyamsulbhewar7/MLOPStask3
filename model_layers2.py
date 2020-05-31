a4f7082032ae32421d5dc33f2fac892bdfcd5cec
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-05-31 22:16:27 +0530

    Update model_layers2.py

--- model_layers2.py
+++ model_layers2.py
@@ -1,4 +1,6 @@
 import model_layers
+from model_layers import model
+model = Sequential()
 model.add(Dense(10, activation='softmax'))
 opt = SGD(lr=0.01, momentum=0.9)
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])

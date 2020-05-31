67af344d9ecab9ea6335caa40d3ebb672bbb4fc9
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-05-31 18:27:45 +0530

    model1 created

---
+++ model_layers.py
@@ -0,0 +1,8 @@
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2)))
+	#model.add(Flatten()) #removing this
+	
+  #model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))

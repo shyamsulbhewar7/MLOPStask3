675da577e4f3683e71020e6da0264a8a2267f599
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-05-31 12:40:06 +0530

    fifth comment

--- mnist.py
+++ mnist.py
@@ -1,63 +1,122 @@
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.models import Sequential
+from keras import metrics
+from keras.layers import Softmax
+from keras.optimizers import Adam
+import random
 
-import tensorflow as tf 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+def conv1():
+    return (Convolution2D(filters=random.randint(30,50), 
+                        kernel_size=(3,3), 
+                        activation='relu',
+                   input_shape=(128, 128, 3)
+                       ))
+
+def conv2():
+    return (Convolution2D(filters=random.randint(30,50), 
+                        kernel_size=random.choice(((3,3),(4,4),(5,5))), 
+                        activation='relu',) )
+
+def max1():
+    return (MaxPooling2D(pool_size=(2, 2)))
+
+def dense1():
+    return (Dense(units=128, activation='relu'))
+
+def dense2():
+    return (Dense(units=64, activation='relu'))
+
+def dense3():
+    return (Dense(units=32, activation='relu'))
+
+def dense4():
+    return (Dense(units=1, activation='relu'))
+
+model=Sequential()
+
+model.add(conv1())
+model.add(max1())
+X = random.randint(1,5)
+if X==1:
+    model.add(conv2())
+    model.add(max1())
+elif X==2:
+    model.add(conv2())
+    model.add(max1())
+    model.add(Flatten())
+    model.add(dense1())
+elif X==3:
+    model.add(conv2())
+    model.add(max1())
+    model.add(Flatten())
+    model.add(dense1())
+    model.add(dense3())
+elif X==4:
+    model.add(conv2())
+    model.add(max1())
+    model.add(conv2())
+    model.add(max1())
+    model.add(Flatten())
+    model.add(dense1())
+    model.add(dense3())
+else:
+    model.add(conv2())
+    model.add(max1())
+    model.add(conv2())
+    model.add(max1())
+    model.add(Flatten())
+    model.add(dense1())
+    model.add(dense2())
+    model.add(dense3())
+model.add(dense4())
+model.add(Softmax())
 
 
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+print(model.summary())
 
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+model.compile(optimizer=Adam(learning_rate=0.0002), loss='binary_crossentropy', metrics=['accuracy'])
 
-input_shape = (28, 28, 1)
 
-# Making sure that the values are float so that we can get decimal points after division
+from keras_preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory(
+        'cnn_dataset/training_set/',
+        target_size=(128, 128),
+        batch_size=32,
+        class_mode='binary')
+test_set = test_datagen.flow_from_directory(
+        'cnn_dataset/test_set/',
+        target_size=(128, 128),
+        batch_size=32,
+        class_mode='binary')
 
-x_train = x_train.astype('float32')
 
-x_test = x_test.astype('float32')
+# In[10]:
 
-# Normalizing the RGB codes by dividing it to the max RGB value.
 
-x_train /= 255
+history=model.fit(
+        training_set,
+        steps_per_epoch=300,
+        epochs=15,
+        validation_data=test_set,
+        validation_steps=80,)
 
-x_test /= 255
 
-import keras 
-from keras.models import Sequential 
-from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
 
-from keras.layers.normalization import BatchNormalization
-import numpy as np
-model = Sequential()
-model.add(Conv2D(filters= 64, kernel_size=(7,7), strides=(4,4), padding='valid'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-model.add(Conv2D(filters= 64, kernel_size=(4,4), strides=(4,4), padding='valid'))
-model.add(Conv2D(filters= 128, kernel_size=(2,2), strides=(2,2), padding='valid'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-model.add(Conv2D(filters= 256, kernel_size=(2,2), strides=(2,2), padding='valid'))
-model.add(Conv2D(filters= 512, kernel_size=(2,2), strides=(2,2), padding='valid'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-model.add(Conv2D(filters= 1024, kernel_size=(2,2), strides=(2,2), padding='valid'))
-model.add(Dense(4096, input_shape=(224*224*3,)))
-model.add(Activation('relu'))
-# Add Dropout
+print(max(history.history['accuracy']))
+if (max(history.history['accuracy'])) > .80 :
+    model.save('model.h5')
 
-model.add(Dropout(0.4))
-model.add(Dense(2048, input_shape=(224*224*3,)))
-model.add(Activation('relu'))
-# Add Dropout
 
-model.add(Dropout(0.4))
-model.add(Dense(1024, input_shape=(224*224*3,)))
-model.add(Activation('relu'))
-# Add Dropout
-
-model.add(Dropout(0.4))
-model.add(Dense(17)) 
-model.add(Activation('softmax'))
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=["accuracy"]) 
-g = model.fit(x_train,y_train,epochs=10)
-f = model.evaluate(x_test, y_test)
-
-s = open("accuracy.txt","w+") 
-s.write(f)
-s.close()
+fh = open('/root/Task3/accuracy.txt','w+')
+fh.write (str(history.history['accuracy']))
+fh.close()

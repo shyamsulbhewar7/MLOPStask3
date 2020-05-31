588aec6890f1c86adffd818c77fdfdf46b74b1a5
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-05-31 22:25:26 +0530

    Update model_layers2.py

--- model_layers2.py
+++ model_layers2.py
@@ -1,3 +1,14 @@
+from numpy import mean
+from numpy import std
+from sklearn.model_selection import KFold
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
 import model_layers
 from model_layers import *
 model.add(Dense(10, activation='softmax'))
